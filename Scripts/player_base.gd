7c62089c063464de2c299bcbf401da492ed26aab
Hugo Save <hugo.save@outlook.com>
2019-10-26 19:58:25 +0200

    PlayerAttack and enemy damage

--- Scripts/player_base.gd
+++ Scripts/player_base.gd
@@ -4,6 +4,8 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
+var primary_attack_damage = 5
+
 var left_pressed = false
 var right_pressed = false
 var up_pressed = false
@@ -11,14 +13,21 @@ var down_pressed = false
 var player_controller_id
 
 var player_speed = 60
+
+var melee_collision
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	melee_collision = $Melee_Attack/CollisionShape2D
+	melee_collision.disabled = true
 	pass # Replace with function body.
 
 func _process(delta):	
 
 	var dir = get_input()
-	move_and_slide(dir * player_speed)
+	
+	if dir != Vector2(0,0):
+		$Melee_Attack.rotation = dir.angle()
+		move_and_slide(dir * player_speed)
 
 func test():
 	print("test")
@@ -35,8 +44,15 @@ func get_input():
 	
 	return motion_dir.normalized()
 
-
-
+func primary_attack():
+	print("primary attack")
+	$AnimationPlayer.play("attack")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+
+func _on_Melee_Attack_body_entered(body):
+	if body.is_in_group("enemies"):
+		body.take_damage(primary_attack_damage)
--- enemyBase.gd
+++ enemyBase.gd
@@ -10,7 +10,19 @@ var deathAnim = "Death"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("Walk")
+	$Sprite.scale= Vector2(1,1)
 	pass # Replace with function body.
 
 func _process(delta):
 	pass
+
+
+func take_damage(damage):
+	print("hit")
+	health -= damage
+	
+	if health <= 0:
+		$AnimationPlayer.play("Death")
+
+func death():
+	queue_free()
