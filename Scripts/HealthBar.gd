5d816556785bf149d7f8cc19a2dd92fc6e827268
Hugo Save <hugo.save@outlook.com>
2019-10-28 13:28:25 +0100

    dsa

---
+++ Scripts/HealthBar.gd
@@ -0,0 +1,29 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var maxHealth = 100
+onready var health_bar = $TextureProgress
+
+var p1_health = preload("res://Resources/UI/p1Healthbar.png")
+var p2_health = preload("res://Resources/UI/p2Healthbar.png")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func set_to_player_1():
+	health_bar.texture_progress = p1_health
+func set_to_player_2():
+	health_bar.texture_progress = p2_health
+	
+
+func set_max_health(value):
+	maxHealth = value
+	update_health(health_bar.value)
+	
+func update_health(health):
+	health_bar.value = (health/maxHealth) * 100
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
